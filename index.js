5d0c4544a98d597013f14f0e4a015ba550f4dd31
SV <72838881+santis1001@users.noreply.github.com>
2023-05-21 22:42:25 -0600

    Hex Validator

--- index.js
+++ index.js
@@ -20,13 +20,21 @@ const questions = [
         type: 'list',
         name: 'L_ColorSelection',
         message: 'Select wich way to apply color youll like to use. ',
-        choices: ['Predefined Color Keywords','Hexadecimal Color']
+        choices: ['Predefined Color Keywords', 'Hexadecimal Color']
     },
     {
         type: 'input',
         name: 'L_ColorHex',
         message: 'Input the Hexadecimal Color without the #. (Ex: ADADAD)',
         when: (answers) => answers.L_ColorSelection === 'Hexadecimal Color',
+        validate: (input) => {
+            const colorReg = /^([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
+
+            if (!colorReg.test(input)) {
+                return 'Please enter a valid hexadecimal color.';
+            }
+            return true;
+        },
     },
     {
         type: 'list',
@@ -38,7 +46,7 @@ const questions = [
 ]
 
 function init() {
-    inquirer.prompt(questions).then( (answers) =>{
+    inquirer.prompt(questions).then((answers) => {
         console.log(answers);
     });
 }
