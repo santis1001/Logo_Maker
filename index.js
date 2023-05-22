5b50dd0af936bddae0164ebbfaf7c3978fca04f0
SV <72838881+santis1001@users.noreply.github.com>
2023-05-21 23:37:15 -0600

    Shapes objects, constructors, and renders

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const questions = [
         type: 'input',
         name: 'L_text',
         message: 'Enter the text that will be displayed in the logo.',
+        validate: (input) => {
+            if (input.length > 3) {
+                return 'Please enter up to 3 characters.';
+            }
+            return true;
+        }
     },
     {
         type: 'list',
@@ -24,12 +30,11 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'L_ColorHex',
+        name: 'L_Color',
         message: 'Input the Hexadecimal Color without the #. (Ex: ADADAD)',
         when: (answers) => answers.L_ColorSelection === 'Hexadecimal Color',
         validate: (input) => {
             const colorReg = /^([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
-
             if (!colorReg.test(input)) {
                 return 'Please enter a valid hexadecimal color.';
             }
@@ -38,7 +43,7 @@ const questions = [
     },
     {
         type: 'list',
-        name: 'L_ColorPre',
+        name: 'L_Color',
         message: 'Select a color from the Predefine SVG Colors List',
         choices: color,
         when: (answers) => answers.L_ColorSelection === 'Predefined Color Keywords',
@@ -48,6 +53,7 @@ const questions = [
 function init() {
     inquirer.prompt(questions).then((answers) => {
         console.log(answers);
+        const Circle = require('./lib/circle');
     });
 }
 
